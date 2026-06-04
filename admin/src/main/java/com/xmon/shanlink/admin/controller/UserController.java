185b6f184111f1782792ec7e48a554900cd9684f
xiechimon <xiechimon@qq.com>
2026-06-04 13:24:15 +0800

    feat(admin): 实现根据用户名查询用户脱敏信息功能

--- admin/src/main/java/com/xmon/shanlink/admin/controller/UserController.java
+++ admin/src/main/java/com/xmon/shanlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.xmon.shanlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.xmon.shanlink.admin.common.convention.result.Result;
 import com.xmon.shanlink.admin.common.convention.result.Results;
+import com.xmon.shanlink.admin.dto.resp.UserActualRespDTO;
 import com.xmon.shanlink.admin.dto.resp.UserRespDTO;
 import com.xmon.shanlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +20,21 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * 根据用户名查询用户信息
+     * 根据用户名查询脱敏用户信息
      */
     @GetMapping("/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable String username) {
         return Results.success(userService.getUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查询真实用户信息
+     */
+    @GetMapping("/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable String username) {
+        return Results.success(
+                BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class)
+        );
     }
 
 }
