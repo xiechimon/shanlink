a694550d6d70a70b4565763f650257c8c4e428ae
xiechimon <xiechimon@qq.com>
2026-06-27 14:08:55 +0800

    chore(rocketmq): 接入云中间件，添加前缀区分

--- src/main/java/com/xmon/shanlink/common/constant/RocketMQConstant.java
+++ src/main/java/com/xmon/shanlink/common/constant/RocketMQConstant.java
@@ -8,10 +8,10 @@ public class RocketMQConstant {
     /**
      * 短链接统计保存 Topic
      */
-    public static final String STATS_SAVE_TOPIC = "shan-link_stats-save_topic";
+    public static final String STATS_SAVE_TOPIC = "xiechimon-shan-link_stats-save_topic";
 
     /**
      * 短链接统计保存消费者组
      */
-    public static final String STATS_SAVE_CONSUMER_GROUP = "shan-link_stats-save_consumer-group";
+    public static final String STATS_SAVE_CONSUMER_GROUP = "xiechimon-shan-link_stats-save_consumer-group";
 }
